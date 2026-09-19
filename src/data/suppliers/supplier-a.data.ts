7ae6aa9f00b549fe1a1f4210dee9cc9462f68239
Yogesh <chauhanyogesh950@gmail.com>
2026-09-19 17:33:13 +0530

    Add hotels to supplier A and supplier B data files

--- src/data/suppliers/supplier-a.data.ts
+++ src/data/suppliers/supplier-a.data.ts
@@ -1,6 +1,7 @@
 import { SupplierHotel } from '../../types';
 
 export const supplierAHotels: SupplierHotel[] = [
+    // Common with Supplier B
     {
         hotelId: 'a1',
         name: 'Holtin',
@@ -22,4 +23,70 @@ export const supplierAHotels: SupplierHotel[] = [
         city: 'delhi',
         commissionPct: 15,
     },
+    {
+        hotelId: 'a4',
+        name: 'The Imperial',
+        price: 9500,
+        city: 'delhi',
+        commissionPct: 14,
+    },
+    {
+        hotelId: 'a5',
+        name: 'Hyatt Regency',
+        price: 8800,
+        city: 'delhi',
+        commissionPct: 16,
+    },
+    {
+        hotelId: 'a6',
+        name: 'Pride Plaza',
+        price: 5400,
+        city: 'delhi',
+        commissionPct: 11,
+    },
+
+    // Unique to Supplier A
+    {
+        hotelId: 'a7',
+        name: 'ITC Maurya',
+        price: 10500,
+        city: 'delhi',
+        commissionPct: 18,
+    },
+    {
+        hotelId: 'a8',
+        name: 'Leela Palace',
+        price: 11000,
+        city: 'delhi',
+        commissionPct: 17,
+    },
+    {
+        hotelId: 'a9',
+        name: 'Hotel Ashoka',
+        price: 6500,
+        city: 'delhi',
+        commissionPct: 12,
+    },
+    {
+        hotelId: 'a10',
+        name: 'City Park',
+        price: 4200,
+        city: 'delhi',
+        commissionPct: 9,
+    },
+    {
+        hotelId: 'a11',
+        name: 'Royal Residency',
+        price: 6100,
+        city: 'delhi',
+        commissionPct: 13,
+    },
+    {
+        hotelId: 'a12',
+        name: 'Metro Inn',
+        price: 3600,
+        city: 'delhi',
+        commissionPct: 8,
+    },
 ];
+
--- src/data/suppliers/supplier-b.data.ts
+++ src/data/suppliers/supplier-b.data.ts
@@ -1,6 +1,7 @@
 import { SupplierHotel } from '../../types';
 
 export const supplierBHotels: SupplierHotel[] = [
+    // Common with Supplier A
     {
         hotelId: 'b1',
         name: 'Holtin',
@@ -17,9 +18,74 @@ export const supplierBHotels: SupplierHotel[] = [
     },
     {
         hotelId: 'b3',
+        name: 'Taj Palace',
+        price: 7600,
+        city: 'delhi',
+        commissionPct: 19,
+    },
+    {
+        hotelId: 'b4',
+        name: 'The Imperial',
+        price: 9100,
+        city: 'delhi',
+        commissionPct: 15,
+    },
+    {
+        hotelId: 'b5',
+        name: 'Hyatt Regency',
+        price: 8200,
+        city: 'delhi',
+        commissionPct: 18,
+    },
+    {
+        hotelId: 'b6',
+        name: 'Pride Plaza',
+        price: 5100,
+        city: 'delhi',
+        commissionPct: 10,
+    },
+
+    // Unique to Supplier B
+    {
+        hotelId: 'b7',
         name: 'Marriott',
         price: 7000,
         city: 'delhi',
         commissionPct: 18,
+    },
+    {
+        hotelId: 'b8',
+        name: 'Holiday Inn',
+        price: 5800,
+        city: 'delhi',
+        commissionPct: 14,
+    },
+    {
+        hotelId: 'b9',
+        name: 'Airport View',
+        price: 4500,
+        city: 'delhi',
+        commissionPct: 11,
+    },
+    {
+        hotelId: 'b10',
+        name: 'Grand Residency',
+        price: 7300,
+        city: 'delhi',
+        commissionPct: 15,
+    },
+    {
+        hotelId: 'b11',
+        name: 'The Fern',
+        price: 6300,
+        city: 'delhi',
+        commissionPct: 13,
+    },
+    {
+        hotelId: 'b12',
+        name: 'Golden Tulip',
+        price: 4900,
+        city: 'delhi',
+        commissionPct: 12,
     },
 ];
